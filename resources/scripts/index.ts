4cd232eab63c90cf44d15a3592f2ea4de2bee877
a-maged <maged.dev@gmail.com>
2020-12-15 20:36:22 +0200

    added js modules

---
+++ resources/scripts/index.ts
@@ -0,0 +1,26 @@
+import '../styles/style.css';
+import modal from './modules/modal';
+
+const registeredModules: any = { modal };
+const moduleAttr = 'data-js';
+const moduleInitialized = 'data-initialized';
+
+document.querySelectorAll(`:scope [${moduleAttr}]`).forEach((element) => {
+  initializeModules(element);
+});
+
+function initializeModules(elem: any) {
+  const module = elem.getAttribute(moduleAttr);
+
+  if (module === undefined) {
+    throw `Module not defined (use ${moduleAttr}="")`;
+  } else if (module in registeredModules) {
+    if (elem.getAttribute(moduleInitialized) === 'true') return;
+
+    new registeredModules[module](elem);
+
+    elem.setAttribute(moduleInitialized, true);
+  } else {
+    throw `Module ${module} not found`;
+  }
+}
---
+++ resources/scripts/modules/modal.ts
@@ -0,0 +1,7 @@
+export default (element: HTMLElement) => {
+  console.log(element);
+
+  // document.addEventListener('click', function(e) {
+  //     e.target.innerText = 'sss';
+  // });
+};
